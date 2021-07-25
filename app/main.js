70916c60f99c9c64750914fa16a375db41e522fb
EN <iwagner76@gmail.com>
2021-07-25 17:18:10 -0400

    added html render of game

--- app/main.js
+++ app/main.js
@@ -1,12 +1,5 @@
 import Game from "./Game.js";
+import GameView from "./GameView.js";
 
 let game = new Game();
-
-game.makeMove(0);
-game.makeMove(3);
-game.makeMove(7);
-game.makeMove(4);
-game.makeMove(2);
-game.makeMove(8);
-console.log(game.board);
-console.log(game.findWinningCombo());
+let gameView = new GameView(document.getElementById("app"));
