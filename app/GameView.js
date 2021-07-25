bd7c3684eb788a41061d5d34c24a1c456e33bd50
EN <iwagner76@gmail.com>
2021-07-25 18:11:31 -0400

    added css for game and logic for clicking on tile

--- app/GameView.js
+++ app/GameView.js
@@ -4,15 +4,57 @@ export default class GameView {
     this.root.innerHTML = `
       <div class="header">
           <div class="header__turn">
-            X's turn
           </div>
           <div class="header__status">
-            In Progress
+              In Progress
           </div>
           <button type="button" class="header__restart">
               <i class="material-icons">refresh</i>
           </button>
       </div>
+      <div class="board">
+          <div class="board__tile" data-index="0"></div>
+          <div class="board__tile" data-index="1"></div>
+          <div class="board__tile" data-index="2"></div>
+          <div class="board__tile" data-index="3"></div>
+          <div class="board__tile" data-index="4"></div>
+          <div class="board__tile" data-index="5"></div>
+          <div class="board__tile" data-index="6"></div>
+          <div class="board__tile" data-index="7"></div>
+          <div class="board__tile" data-index="8"></div>
+      </div>
     `;
+
+    this.onTileClick = undefined;
+    this.onRestartClick = undefined;
+
+    this.root.querySelectorAll(".board__tile").forEach((tile) => {
+      tile.addEventListener("click", () => {
+        if (this.onTileClick) {
+          this.onTileClick(tile.dataset.index);
+        }
+      });
+    });
+
+    this.root
+      .querySelector(".header__restart")
+      .addEventListener("click", () => {
+        if (this.onRestartClick) {
+          this.onRestartClick();
+        }
+      });
   }
+
+  update(game) {
+    this.updateTurn(game);
+    this.updateStatus(game);
+    this.updateBoard(game);
+  }
+  updateTurn(game) {
+    this.root.querySelector(
+      ".header__turn"
+    ).textContent = `${game.turn}'s turn`;
+  }
+  updateStatus(game) {}
+  updateBoard(game) {}
 }
--- app/main.js
+++ app/main.js
@@ -3,3 +3,14 @@ import GameView from "./GameView.js";
 
 let game = new Game();
 let gameView = new GameView(document.getElementById("app"));
+
+//functions
+gameView.onTileClick = function (i) {
+  console.log(`Tile clicked ${i}`);
+};
+
+gameView.onRestartClick = function () {
+  console.log("Game Restarted");
+};
+
+gameView.update(game);
